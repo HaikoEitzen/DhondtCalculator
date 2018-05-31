e961734bbf0b4e61727b1bafe560e17a58b3b7f3
haiko <haiko.eitzen@gmail.com>
2018-05-31 21:21:07 +0200

    Divided functions into multiple functions and subroutines.

--- dhondt_calculator.py
+++ dhondt_calculator.py
@@ -2,26 +2,36 @@
 
 # d'Hondt seating allocation method
 def compute(nr_of_seats, vote_counts):
+    grid = generate_grid(nr_of_seats, vote_counts)
+    seat_allocation = compute_seat_allocation(grid)
+    return seat_allocation
+
+
+def generate_grid(nr_of_seats, vote_counts):
     # generate grid of vote quotient according to formula V/(s + 1)
     # where V is the number of votes of a given party and s the number of seats already awarded
     grid = [[int(votes / (seats_awarded + 1)) for seats_awarded in range(nr_of_seats)] for votes in vote_counts]
-    # list of number of seats awarded to each party
-    seats = [0] * len(vote_counts)
-    # compute number of seats
+    return grid
+
+
+def compute_seat_allocation(grid):
+    vote_counts = len(grid)
+    seat_allocation = [0] * len(vote_counts)
+    allocate_seats(seat_allocation, grid)
+    return seat_allocation
+
+
+def allocate_seats(seat_allocation, grid):
+    nr_of_seats = len(grid[0])
     for seat in range(nr_of_seats):
-        seats[next_seat(grid)] += 1
-    return seats
+        seat_allocation[next_seat(grid)] += 1
 
 
-# obtain next seat given computed vote grid
 def next_seat(grid):
-    # find the maximum element in the grid
     current_max = -1
-    for index, row in enumerate(grid):
-        for elem in row:
-            if elem > current_max:
-                current_max, max_index = elem, index
-    # remove maximum from grid
-    grid[max_index].remove(current_max)
-    # return corresponding row (party/movement)
-    return max_index
+    for party, vote_quotients in enumerate(grid):
+        for vq in vote_quotients:
+            if vq > current_max:
+                current_max, party_that_won_seat = vq, party
+    grid[party_that_won_seat].remove(current_max)
+    return party_that_won_seat
